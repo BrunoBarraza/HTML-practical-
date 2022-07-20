1199bbafd3181d5b5d4b02032585a88c25977698
BrunoBarraza <bruno.barraza@atos.net>
2022-07-20 15:31:53 -0500

    Minify JS, CSS, and HTML in production

--- Blunder Practice/wp bundlers/webpack.prod.js
+++ Blunder Practice/wp bundlers/webpack.prod.js
@@ -2,6 +2,9 @@ const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: "production",
@@ -9,5 +12,35 @@ module.exports = merge(common, {
         filename: "[name].[contenthash].bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
-    plugins: [new CleanWebpackPlugin()]
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, //3. Extract css into files
+                    "css-loader", //2. Turns css into commonjs
+                    "sass-loader" //1. Turns sass into css
+                ]
+            },
+        ]
+    },
+    optimization: {
+        minimizer: [
+          // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+          // `...`,
+          new CssMinimizerPlugin(),
+          new HtmlWebpackPlugin({
+            template: "./src/template.html",
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                removeComments: true,
+            }
+        }),
+        ],
+      },
 });
