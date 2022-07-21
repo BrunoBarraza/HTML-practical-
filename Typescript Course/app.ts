cba2c138093cd1f899ff8447b7c5655a7db49910
BrunoBarraza <bruno.barraza@atos.net>
2022-07-21 15:47:36 -0500

    Union Types

--- Typescript Course/app.ts
+++ Typescript Course/app.ts
@@ -1,48 +1,16 @@
-// const person : {
-//     name: string;
-//     age: number;
-// } = {
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string]; // Special Array with exactly two elements, first a number and second a string.
-// } = {
-//     name: 'Bruno',
-//     age: 25,
-//     hobbies: ['Sports', 'Cooking'],
-//     role: [2, 'author']
-// };
-
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-// Start with an upper case letter since its a custom type. Most enums have all uppercase values.
-enum Role {ADMIN, READ_ONLY, AUTHOR}; // ADMIN = 0, and so forth, you can change the starting number or make mapped custom types.
-
-const person = {
-    name: 'Bruno',
-    age: 25,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-
-// person.role.push('admin'); // This is an exception on Tuples.
-// person.role[1] = 10;
-
-// person.role [0, 'admin', 'user'];
-
-let favoriteaActivities: string[];
-favoriteaActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); // !! ERROR !! map does work on Arrays but not on Strings.
+// Union types.
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
-}
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combineNames = combine("Max", "Anna");
+console.log(combineNames);
