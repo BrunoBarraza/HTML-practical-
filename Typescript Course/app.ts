7fa8bdb8ac2d97131e55f84434e993827ba27fc5
BrunoBarraza <bruno.barraza@atos.net>
2022-07-21 15:28:43 -0500

    Tuples Example

--- Typescript Course/app.ts
+++ Typescript Course/app.ts
@@ -2,11 +2,22 @@
 //     name: string;
 //     age: number;
 // } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]; // Special Array with exactly two elements, first a number and second a string.
+} = {
     name: 'Bruno',
     age: 25,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+
+// person.role.push('admin'); // This is an exception on Tuples.
+// person.role[1] = 10;
+
+// person.role [0, 'admin', 'user'];
 
 let favoriteaActivities: string[];
 favoriteaActivities = ['Sports'];
