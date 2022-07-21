53c9a05433ce39ff434b54369d81baa7d0c7d04c
BrunoBarraza <bruno.barraza@atos.net>
2022-07-21 13:02:09 -0500

    Array Types

--- Typescript Course/app.ts
+++ Typescript Course/app.ts
@@ -4,7 +4,16 @@
 // } = {
 const person = {
     name: 'Bruno',
-    age: 25
+    age: 25,
+    hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteaActivities: string[];
+favoriteaActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !! ERROR !! map does work on Arrays but not on Strings.
+}
