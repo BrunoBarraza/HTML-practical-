a6964ba9b16ed2d4341a906afac5be1f142d3a72
BrunoBarraza <bruno.barraza@atos.net>
2022-07-22 08:01:03 -0500

    Final Commit

--- Typescript Course/app.ts
+++ Typescript Course/app.ts
@@ -1,15 +1,3 @@
-// The Unknown Type.
-let userInput: unknown; // its more restrictive than any.
-let userName: string;
+const userName = 'Maximilian';
 
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string") {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
-
-generateError('An Error Ocurred!', 500);
+console.log(userName);
