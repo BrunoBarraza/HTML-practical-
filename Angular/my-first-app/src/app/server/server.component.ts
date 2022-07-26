555937bd332ee62bd6fb8f429272ecf95b15c4e7
BrunoBarraza <bruno.barraza@atos.net>
2022-07-26 12:12:27 -0500

    Styling and outputting elements with ng

--- Angular/my-first-app/src/app/server/server.component.ts
+++ Angular/my-first-app/src/app/server/server.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
+import { retry } from 'rxjs';
 
 @Component({
   selector: 'app-server', //HTML Tag
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
   serverId = 10;
   serverStatus = 'offline';
 
-  getServerStatus(){
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
