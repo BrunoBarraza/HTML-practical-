5daf7cea6f65cc52bb873f1e1fe3a2853ace99c0
BrunoBarraza <bruno.barraza@atos.net>
2022-07-27 12:26:23 -0500

    ViewChild access

--- Angular/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ Angular/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
 
 @Component({
   selector: "app-cockpit",
@@ -7,32 +14,34 @@ import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 })
 // Events we can emmit
 export class CockpitComponent implements OnInit {
-  @Output() serverCreated = new EventEmitter<{
-    serverName: string;
-    serverContent: string;
-  }>(); 
-  @Output() blueprintCreated = new EventEmitter<{
+  @Output("srvCreated") serverCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
   }>();
-  newServerName = "";
-  newServerContent = "";
-
-  onAddServer() {
-    this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
-    });
-  }
-
-  onAddBlueprint() {
-    this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
-    });
-  }
+  @Output("bpCreated") blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  // newServerName = "";
+  // newServerContent = "";
+  @ViewChild("serverContentInput", { static: true })
+  serverContentInput: ElementRef;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
+    });
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
+    });
+  }
 }
