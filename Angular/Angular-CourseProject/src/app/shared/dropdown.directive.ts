1a51de1e82b8d3fa8e69268637661ffd6ed69f7d
BrunoBarraza <bruno.barraza@atos.net>
2022-07-27 16:25:06 -0500

    End of Directives

---
+++ Angular/Angular-CourseProject/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,21 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  // document.click allows the dropdown to be closed by a click anywhere
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
+  }
+  constructor(private elRef: ElementRef) {}
+}
