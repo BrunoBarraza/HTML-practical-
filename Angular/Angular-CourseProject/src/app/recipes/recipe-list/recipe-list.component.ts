09b4a80746d63fc42f5528c2982ca15a307e609d
BrunoBarraza <bruno.barraza@atos.net>
2022-07-27 14:35:11 -0500

    End of Components and Databinding

--- Angular/Angular-CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/Angular-CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,15 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
+      'This is simply a test',
+      'https://www.cookipedia.co.uk/wiki/images/4/40/Sourdough_bread_pizza_recipe.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
       'This is simply a test',
       'https://www.cookipedia.co.uk/wiki/images/4/40/Sourdough_bread_pizza_recipe.jpg'
     ),
@@ -18,4 +24,8 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
