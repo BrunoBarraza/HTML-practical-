14e494927e55ed162dc38ed5f77944743c7bc354
BrunoBarraza <bruno.barraza@atos.net>
2022-07-27 13:33:12 -0500

    Final commit for Components and Databinding

--- Angular/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ Angular/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, ViewChild, ElementRef, ContentChild } from "@angular/core";
 
 @Component({
   selector: "app-server-element",
@@ -6,7 +6,10 @@ import { Component, OnInit, Input } from "@angular/core";
   styleUrls: ["./server-element.component.css"],
 })
 export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: { type: string; name: string; content: string }; //Any parent component can bind to this property now.
+  @Input("srvElement") element: { type: string; name: string; content: string }; //Any parent component can bind to this property now.
+  @Input() name: string;
+  @ViewChild("heading", { static: true }) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
 
   constructor() {}
 
