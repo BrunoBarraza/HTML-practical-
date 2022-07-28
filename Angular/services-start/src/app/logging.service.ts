b362a1c0fde4bd2acd181954695aa36ffe82399e
BrunoBarraza <bruno.barraza@atos.net>
2022-07-28 11:45:48 -0500

    Injecting services into services

--- Angular/services-start/src/app/logging.service.ts
+++ Angular/services-start/src/app/logging.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from "@angular/core";
+
+@Injectable() // Not necessary right now, but a good practice for the future.
 export class LoggingService {
   logStatusChange(status: string) {
     console.log("A server status changed, new status: " + status);
